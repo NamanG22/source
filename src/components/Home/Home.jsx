fb56ce806e70ef91b57aaba8d8bb29ef2cea3194
Naman <t-naman1.garg@ocltp.com>
2026-03-21 01:41:09 +0530

    section 3 removed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,6 @@ import PageMask from '../PageMask/PageMask'
 import Hero from '../Hero/Hero'
 import Section1 from '../Section1/Section1'
 import Section2 from '../Section2/Section2'
-import Section3 from '../Section3/Section3'
 import Section4 from '../Section4/Section4'
 import IndiaVsHubs from '../IndiaVsHubs/IndiaVsHubs'
 import WhySourceCentral from '../WhySourceCentral/WhySourceCentral'
@@ -27,7 +26,6 @@ function Home() {
       <Hero />
       <Section1 />
       <Section2 />
-      <Section3 />
       <Section4 />
       <IndiaVsHubs />
       <WhySourceCentral />
